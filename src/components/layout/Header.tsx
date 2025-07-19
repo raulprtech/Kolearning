6122984137ba8efab1f4b2d0c2e119b670b68357
Raul Alberto Pacheco Rodriguez <raulavenger21@gmail.com>
2025-07-19 05:55:04 +0000

    prueba con este emoji: 🪙

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -23,23 +23,8 @@ async function getUserData(uid: string): Promise<User | null> {
     };
 }
 
-const CoinIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      width="24" 
-      height="24" 
-      viewBox="0 0 24 24" 
-      fill="none" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
-      strokeLinejoin="round" 
-      {...props}
-    >
-        <circle cx="12" cy="12" r="8"></circle>
-        <path d="M12 16v-4"></path>
-        <path d="M12 8h.01"></path>
-    </svg>
+const CoinIcon = (props: React.HTMLAttributes<HTMLSpanElement>) => (
+    <span {...props}>🪙</span>
 );
 
 
