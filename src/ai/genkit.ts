3da966a5dfe01f15ad3b6575b9bfe8aa7bb0aff2
Raul Alberto Pacheco Rodriguez <raulavenger21@gmail.com>
2025-07-22 20:31:56 +0000

    Creo que ya esta disponible la versión estable del modelo "gemini-flash

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,7 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-flash-lite-preview-06-17',
+  model: 'googleai/gemini-1.5-flash',
   telemetry: {
     instrumentation: false,
   },
