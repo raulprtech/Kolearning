731b0d21a86b64849486722f43856f73fb79404a
Raul Alberto Pacheco Rodriguez <raulavenger21@gmail.com>
2025-07-23 05:38:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -35,3 +35,5 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
+
+export const runtime = 'nodejs';
