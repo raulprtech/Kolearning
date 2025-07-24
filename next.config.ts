cbf777fc107a25cc0184ee39a0471bc9c482d11a
Raul Alberto Pacheco Rodriguez <raulavenger21@gmail.com>
2025-07-24 02:20:04 +0000

    seguimos igual

--- next.config.ts
+++ next.config.ts
@@ -7,23 +7,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   serverExternalPackages: ['firebase-admin'],
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'unsafe-none',
-          },
-        ],
-      },
-    ]
-  },
   webpack: (config, { isServer }) => {
     // Ignorar warnings de genkit/opentelemetry en el cliente
     if (!isServer) {
