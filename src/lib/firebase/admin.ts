1acd3aa6c4ffdb8499fc441ec2ad2c57571cd3fa
Raul Alberto Pacheco Rodriguez <raulavenger21@gmail.com>
2025-07-24 00:07:16 +0000

    Desplegué en vercel pero tengo este error. Ya esta agregado en los sitio

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,15 +1,27 @@
-'server-only';
+'use only';
 
-import { getApps, initializeApp, App } from 'firebase-admin/app';
+import { getApps, initializeApp, App, cert, ServiceAccount } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
 let app: App;
 
+const serviceAccount: ServiceAccount | undefined = process.env.GOOGLE_PRIVATE_KEY ? {
+    projectId: process.env.GOOGLE_PROJECT_ID,
+    clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+} : undefined;
+
+
 if (!getApps().length) {
-  // Initialize without credentials.
-  // This is the recommended way for environments like Cloud Run and other GCP services.
-  app = initializeApp();
+  if (serviceAccount) {
+    app = initializeApp({
+        credential: cert(serviceAccount)
+    });
+  } else {
+    // This is the recommended way for environments like Cloud Run and other GCP services if GOOGLE_APPLICATION_CREDENTIALS is set
+    app = initializeApp();
+  }
 } else {
   app = getApps()[0];
 }
