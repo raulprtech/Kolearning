48e3d81f1eb83a387ddc57c4d9be0f2582cab97f
Raul Alberto Pacheco Rodriguez <raulavenger21@gmail.com>
2025-08-03 17:07:09 +0000

    este es logo de kolearning, cambia el color primario de la pagina para e

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,16 +4,17 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
+      viewBox="0 0 100 100"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2.5"
+      strokeWidth="4"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M4 4l8 8-8 8" />
-      <path d="M12 4l8 8-8 8" />
+        <path d="M25 85V15H45V35L35 25L45 35V55L35 45L45 55V85H25Z" />
+        <path d="M45 55L65 35L85 55L65 75L45 55Z" style={{ strokeWidth: 4 }}/>
+        <path d="M65 35L80 20" style={{ strokeWidth: 4 }}/>
     </svg>
   )
 }
